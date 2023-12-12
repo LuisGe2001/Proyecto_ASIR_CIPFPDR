e3fde7e560dbf8c54c5994497c1822d08639a8d7
luisge2001 <lggpxd@gmail.com>
2023-12-12 12:52:34 +0100

    Borrado todos los archivos

--- readme.md
+++
@@ -1,2 +0,0 @@
-Proyecto en construccion... 10/12/2023
-Fixed 12/12/2023
--- scripts_master/instalacion_java.sh
+++
@@ -1,4 +0,0 @@
-rpm -ivh https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.rpm
-export JAVA_HOME=/usr/lib/jvm/jdk-11
-export PATH=$JAVA_HOME/bin:$PATH
-source ~/.bashrc
